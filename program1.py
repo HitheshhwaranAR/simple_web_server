c867a7c07066308559119b49dff901596f0004e0
HitheshhwaranAR <jayaramkumar1983@gmail.com>
2025-03-07 11:21:33 +0530

    hi

--- program1.py
+++ program1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -22,20 +21,3 @@ server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-=======
-import http.server
-import socketserver
-
-PORT = 8000
-
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"<html><body><h1>Hitheshh</h1></body></html>")
-
-with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    print(f"Serving at port {PORT}")
-    httpd.serve_forever()
->>>>>>> fb524aa9ec27672dfcfd82331675d5f822e3b850
